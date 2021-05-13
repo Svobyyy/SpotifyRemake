e39ecf752c6c445a47b44f40ac1f68856eaee820
Matěj Svoboda <svobymates@gmail.com>
2021-05-13 04:19:56 +0200

    NavBar

---
+++ script.js
@@ -0,0 +1,32 @@
+let burger = document.querySelector(`.burger`);
+let cara = document.querySelectorAll(`.cara`);
+let menu = document.querySelector(`.menu`);
+let body = document.querySelector(`body`);
+
+burger.addEventListener(`click`, () => {
+    body.classList.add(`scrollStop`)
+    for(let i = 0; i < cara.length; i++)
+    {
+        cara[i].classList.toggle(`animace`);
+    }
+
+    if(menu.classList.contains(`hide`))
+    {
+        menu.classList.add(`fadeInMenu`);
+        menu.classList.remove(`fadeOutMenu`);
+        menu.classList.remove(`hide`);
+    }
+    else {
+        menu.classList.remove(`fadeInMenu`);
+        menu.classList.add(`fadeOutMenu`);
+        menu.addEventListener(`animationend`, () => {
+            if(menu.classList.contains(`fadeOutMenu`))
+            {
+                menu.classList.add(`hide`);
+                body.classList.remove(`scrollStop`)
+            }
+        })
+    }
+
+
+})
